3df52d754ecaf5cf38fd6bef3fc199c9da02c5b2
Bombothula  Dwaraka Sri Sai pradeep <142149881+Saipradeep456@users.noreply.github.com>
2025-01-09 21:52:29 +0530

    Add files via upload

--- even & odd .py
+++ even & odd .py
@@ -14,3 +14,37 @@ else:
 
 # enter a number:5
 # It is odd number
+
+
+
+
+# 3. Even or Odd Checker: Check if a number is even or odd. without using % symbol.
+
+num = int(input("enter a number:"))
+if (num//2)*2 == num :
+  print(f"the number {num} is even")
+else:
+  print(f"the number {num} is odd")
+
+
+#output 
+# enter a number:2
+# the number 2 is even
+
+# enter a number:5
+# the number 5 is odd
+
+
+
+
+
+#   Example 1: num = 8
+
+# 8 // 2 = 4
+# 4 * 2 = 8
+# 8 == 8 is True, so the number is even.
+# Example 2: num = 9
+
+# 9 // 2 = 4 (the decimal part .5 is discarded)
+# 4 * 2 = 8
+# 8 == 9 is False, so the number is odd.
