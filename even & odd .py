c0bc94950baf11f77fbb88956b70508f6aed207d
Bombothula  Dwaraka Sri Sai pradeep <142149881+Saipradeep456@users.noreply.github.com>
2025-01-09 21:42:46 +0530

    Add files via upload

---
+++ even & odd .py
@@ -0,0 +1,16 @@
+# 3. Even or Odd Checker: Check if a number is even or odd.
+
+num = int(input("enter a number:"))
+if num%2 == 0:
+  print("It is even number")
+else:
+  print("It is odd number")
+
+
+#output 
+
+# enter a number:2
+# It is even number
+
+# enter a number:5
+# It is odd number
