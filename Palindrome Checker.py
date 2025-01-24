e1ff5a885eb0eb87275e3d514d9bdab5fb132778
Bombothula  Dwaraka Sri Sai pradeep <142149881+Saipradeep456@users.noreply.github.com>
2025-01-24 21:43:14 +0530

    Add files via upload

---
+++ Palindrome Checker.py
@@ -0,0 +1,18 @@
+# 9. Palindrome Checker: Check if a string is a palindrome.
+
+inp = input("enter a string  :")
+reverse = inp [::-1]
+if reverse == inp:
+  print("it is palindrome")
+else:
+  print("it is not palindrome")
+
+
+# OUTPUT :-
+
+# enter a string  :mom
+# it is palindrome
+
+
+# enter a string  :SAI
+# it is not palindrome
