a534245876954594eeee928783261e1c24e8f117
Bombothula  Dwaraka Sri Sai pradeep <142149881+Saipradeep456@users.noreply.github.com>
2025-01-26 22:02:43 +0530

    Add files via upload

---
+++ Reverse a String.py
@@ -0,0 +1,9 @@
+# 1. Reverse a String: Reverse the characters in a string.
+reverse = input("enter a string:-")
+sai = reverse[::-1]
+print(sai)
+
+# OUTPUT:-
+
+# enter a string:saipradeep
+# # peedarpias
